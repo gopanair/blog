41929435f7e91928e83ec44f9b5854a1702bb986
Gopakumar Nair <gopanair@gmail.com>
2020-04-18 18:35:12 -0400

    changed bg

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,3 @@
 module.exports = {
-  important: true,
-  theme: {
-    container: {
-      center: true,
-      padding: '2rem',
-    },
-    
-  },
-  variants: {
-    opacity: ['responsive', 'hover'],
-    transitionProperty: ['responsive', 'hover', 'focus'],
-  }
+  
 }
